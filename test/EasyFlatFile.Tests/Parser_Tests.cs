7ac0948f8bcc31dd87d382819f1ec83e5cc143d5
Edgars Pivovarenoks <edgarsp@jitt.lv>
2016-12-12 13:48:55 +0200

    xunit tests added WIP

---
+++ test/EasyFlatFile.Tests/Parser_Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EasyFlatFile.Tests
+{
+    public class Parser_Tests
+    {
+        public Parser_Tests()
+        {
+        }
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
